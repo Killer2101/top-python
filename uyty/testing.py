f58029c2765880f0b592070177f6e0958b405b3c
Killer2101 <146407084+Killer2101@users.noreply.github.com>
2025-04-25 10:46:38 +0500

    ts

---
+++ uyty/testing.py
@@ -0,0 +1,11 @@
+def capitalize(text):
+    first_char = text[0].upper()
+    rest_substring = text[1:]
+    return f'{first_char}{rest_substring}'
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно')
+
+if capitalize('') != '':
+    raise Exception('Тесьы не выполнен')
+print ('Тест пройден')
